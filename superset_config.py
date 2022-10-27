255597e32f9f270d96c77e10c8f136a23a292de1
Fredrik Boulund <boulund@users.noreply.github.com>
2022-10-27 09:12:32 +0200

    Set GENERIC_CHART_AXES = True

--- superset_config.py
+++ superset_config.py
@@ -4,3 +4,6 @@ SECRET_KEY = "replace_me"
 # Custom metadata DB URI -- currently not used, here for future reference.
 # SQLALCHEMY_DATABASE_URI = 'postgresql://root:password@hostname/applicationdb'
 
+FEATURE_FLAGS = {
+    "GENERIC_CHART_AXES": True,
+}
